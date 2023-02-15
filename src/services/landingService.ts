f3cd436cea050b1e6dc71c5f71d377088da3b9f3
Jack Sari <janasarii@gmail.com>
2023-02-15 03:01:11 -0500

    add

--- src/services/landingService.ts
+++ src/services/landingService.ts
@@ -14,5 +14,59 @@ class landingService {
       return [];
     }
   }
+
+  async getTestimonials() {
+    try {
+      const response = await fetch(`${url}/testimonials`);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      //   if (request.isAxiosError(err) && err.response) {
+      //     return err.response?.data as TodoErrorResponse;
+      //   }
+      return [];
+    }
+  }
+
+  async getAlliances() {
+    try {
+      const response = await fetch(`${url}/alliances`);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      //   if (request.isAxiosError(err) && err.response) {
+      //     return err.response?.data as TodoErrorResponse;
+      //   }
+      return [];
+    }
+  }
+
+  async getBenefits() {
+    try {
+      const response = await fetch(`${url}/benefits`);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      //   if (request.isAxiosError(err) && err.response) {
+      //     return err.response?.data as TodoErrorResponse;
+      //   }
+      return [];
+    }
+  }
+
+  async getStatistics() {
+    try {
+      const response = await fetch(`${url}/statistics`);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      //   if (request.isAxiosError(err) && err.response) {
+      //     return err.response?.data as TodoErrorResponse;
+      //   }
+      return [];
+    }
+  }
+
+  //
 }
 export default new landingService();
