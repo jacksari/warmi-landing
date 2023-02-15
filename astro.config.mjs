e665d358ba78cfdcb998fcfb8d486f1460b3676c
Jack Sari <janasarii@gmail.com>
2023-02-15 01:28:12 -0500

    add vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({
-        config: { path: './tailwind.config.cjs' }
-    })],
+  integrations: [
+    vue({}),
+    tailwind({
+      config: {
+        path: "./tailwind.config.cjs",
+      },
+    }),
+  ],
 });
---
+++ src/components/vue/FormLanding.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const form = ref({
+  email: "",
+  name: "",
+  message: "",
+});
+
+const click = () => {
+  // console.log('ADD', form.value);
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log('data', json));
+};
+</script>
+
+<template>
+  <div class="bg-red-300">
+    <h2>Formulario</h2>
+    <input type="text" v-model="form.name" />
+    <button @click="click" class="bg-primar">Add</button>
+    <!-- {{ form.name }} -->
+  </div>
+</template>
