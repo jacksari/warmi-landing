971f2eddb05b18affb7f5b5170e108921d97eeae
Jack Sari <janasarii@gmail.com>
2023-05-09 08:51:48 -0500

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    assets: true,
+  },
   integrations: [
     vue({}),
     tailwind({
