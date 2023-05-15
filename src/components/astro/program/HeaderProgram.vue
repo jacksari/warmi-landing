7d08e6a08681eb0c94fe36c2cd50eae0b7e76d75
Jack Sari <janasarii@gmail.com>
2023-05-15 00:47:25 -0500

    add

---
+++ src/components/astro/program/HeaderProgram.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+const data = [
+  {
+    img: "/images/p1.png",
+    title: "Mentoría",
+    description:
+      "Los proyectos tendrán acceso a sesiones individuales con un mentor para apoyarlos en su consolidación del modelo de negocio",
+  },
+  {
+    img: "/images/p2.png",
+    title: "Talleres ",
+    description:
+      " Se realizarán talleres grupales previo diagnóstico de cada emprendimiento, las cuales fortalecerán sus conocimientos y se implementarán en sus propios emprendimiento.",
+  },
+  {
+    img: "/images/p3.png",
+    title: "Mentoría",
+    description:
+      "Monitoreo por parte del equipo de la Incubadora para fortalecer los conocimientos adquiridos en los talleres, haciendo cumplir la implantación en sus emprendimientos.",
+  },
+  {
+    img: "/images/p4.png",
+    title: "Mentoría",
+    description:
+      "La posibilidad de presentar sus proyectos frente a la red de mentores, y público en general, al finalizar el programa.",
+  },
+];
+</script>
+
+<template>
+  <div class="bg-secondary-300">
+    <div class="py-20 w-full mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">
+      <!-- <HeaderSection title="Programas que impulsan tu negocio" /> -->
+
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-6 w-full mt-12"
+      >
+        <div class="col-span-2">
+          <span class="span-valid px-2 py-1 rounded-md">Valida tu idea</span>
+          <h1 class="text-5xl">Preincubación</h1>
+        </div>
+        <div>
+          <p class="font-light">
+            El Programa de pre-Incubación está dirigido a equipos liderados por
+            mujeres o que en su totalidad sean mujeres que tengan una idea de
+            negocio relacionado con startups o emprendimientos de alto impacto.
+            Se enfoca en capacitación, asesoría y mentoría de startups en edad
+            temprana y busca identificar y fomentar proyectos de alto impacto
+            con un fuerte enfoque en innovación, liderazgo, sostenibilidad y
+            tecnología.
+          </p>
+          <p class="font-light mt-8">
+            El programa busca dar un soporte inicial a los emprendedores en el
+            desarrollo de sus ideas de negocio para que puedan llegar a
+            establecer su startup y lanzarla al mercado.
+          </p>
+
+          <div class="mt-10">
+            <a
+              class="text-black font-medium border-2 border-black rounded-md px-14 py-3 bg-white"
+              href="/"
+              >Tengo una idea</a
+            >
+          </div>
+        </div>
+
+        <div class="bg-white p-4">
+          <img
+            class="p-0 m-0 w-full bg-cover"
+            src="/images/program.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="w-full mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 container-footer mt-24">
+      <div class="grid grid-cols-1 md:grid-cols-4 bg-white container-details">
+        <div class="p-10" v-for="(item, index) in data" :key="index">
+          <img class="h-12 w-12 object-cover" :src="item.img" alt="" />
+          <h3 class="font-bold mt-2 mb-2">{{ item.title }}</h3>
+          <p class="font-light text-base leading-5">{{ item.description }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.span-valid {
+  background: #fff3d6;
+  font-size: 12px;
+  border: 1px solid #333333;
+}
+.container-details {
+  border: 1px solid #333333;
+}
+
+.container-footer {
+  position: relative;
+  width: 100%;
+  height: 100px;
+  .container-details {
+    position: absolute;
+    top: -50%;
+    left: 0;
+  }
+}
+</style>
---
+++ src/components/astro/program/SectionsProgram.vue
@@ -0,0 +1,177 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const sections = ref([
+  {
+    title: "introducción al emprendimiento",
+    description:
+      "Introducir a los participantes en lo realmente necesario para entender el ecosistema de innovación y emprendimiento.",
+    lessons: [
+      "Conceptos claves del I&E",
+      "Test del emprendedor",
+      " Enfoque del equipo",
+      "Mindsets del emprendedor",
+    ],
+    active: false,
+  },
+  {
+    title: "Conociendo más sobre la problemática a resolver",
+    description: "Entender la forma de cómo plantear una problemática real.",
+    lessons: [
+      " Mitos y errores del emprendimiento",
+      " Técnica del árbol de problemas",
+      " Mapa de audiencia",
+      "Conceptos claves del I&E",
+      "Test del emprendedor",
+      " Enfoque del equipo",
+      "Mindsets del emprendedor",
+      "Conceptos claves del I&E",
+      "Test del emprendedor",
+      " Enfoque del equipo",
+      "Mindsets del emprendedor",
+    ],
+    active: false,
+  },
+  {
+    title: "Conociendo más sobre la problemática a resolver",
+    description: "Entender la forma de cómo plantear una problemática real.",
+    lessons: [
+      " Mitos y errores del emprendimiento",
+      " Técnica del árbol de problemas",
+      " Mapa de audiencia",
+      "Conceptos claves del I&E",
+      "Test del emprendedor",
+      " Enfoque del equipo",
+      "Mindsets del emprendedor",
+    ],
+    active: false,
+  },
+  {
+    title: "Conociendo más sobre la problemática a resolver",
+    description: "Entender la forma de cómo plantear una problemática real.",
+    lessons: [
+      " Mitos y errores del emprendimiento",
+      " Técnica del árbol de problemas",
+      " Mapa de audiencia",
+      "Conceptos claves del I&E",
+      "Test del emprendedor",
+      " Enfoque del equipo",
+      "Mindsets del emprendedor",
+      "Conceptos claves del I&E",
+      "Test del emprendedor",
+      " Enfoque del equipo",
+      "Mindsets del emprendedor",
+      "Conceptos claves del I&E",
+      "Test del emprendedor",
+    ],
+    active: false,
+  },
+  {
+    title: "Conociendo más sobre la problemática a resolver",
+    description: "Entender la forma de cómo plantear una problemática real.",
+    lessons: [
+      " Mitos y errores del emprendimiento",
+      " Técnica del árbol de problemas",
+      " Mapa de audiencia",
+      "Conceptos claves del I&E",
+      "Test del emprendedor",
+      " Enfoque del equipo",
+    ],
+    active: false,
+  },
+]);
+</script>
+
+<template>
+  <div class="bg-primary-500 pt-24">
+    <div
+      class="py-20 w-full mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 text-center"
+    >
+      <h2 class="text-3xl text-center mt-20 font-medium">
+        Contenido del programa
+      </h2>
+
+      <div class="container-sections mx-auto flex flex-col items-center mt-12">
+        <article
+          class="item-section"
+          v-for="(section, index) in sections"
+          :key="index"
+        >
+          <div class="title-section" @click="section.active = !section.active">
+            <h4 class="font-bold">{{ section.title }}</h4>
+            <svg class="title-icon" :class="section.active ? 'active' : ''" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+              <path
+                d="M12 17.414 3.293 8.707l1.414-1.414L12 14.586l7.293-7.293 1.414 1.414L12 17.414z"
+              />
+            </svg>
+          </div>
+          <div
+            class="container-section text-left"
+            :style="{
+              height: section.active
+                ? `calc(40px + ${section.lessons.length * 28}px)`
+                : '0px',
+            }"
+          >
+            <div style="padding: 1rem; padding-top: 0">
+              <p
+                class="font-light leading-4 mb-2 flex items-center"
+                style="height: 36px"
+              >
+                {{ section.description }}
+              </p>
+              <ul
+                v-for="(lesson, indexLesson) in section.lessons"
+                :key="indexLesson"
+              >
+                <li class="font-light list-disc ml-8">{{ lesson }}</li>
+              </ul>
+            </div>
+          </div>
+        </article>
+
+      
+      </div>
+
+      <div class="mt-12">
+        <button class="text-black font-medium border-2 border-black rounded-md px-14 py-3 bg-secondary-500">Quiero participar</button>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.item-section {
+  width: 50%;
+  border: 1px solid #333333;
+  border-bottom: none;
+  &:last-child {
+    border-bottom: 1px solid #333333;
+  }
+
+  .title-section {
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    cursor: pointer;
+    .title-icon{
+        transition: .4s all ease-in-out;
+    }
+    .active{
+        transform: rotate(180deg);
+    }
+  }
+  .container-section {
+    height: 0;
+    min-height: 0;
+    overflow: hidden;
+    transition: 0.4s all ease-in-out;
+    // padding: 1rem;
+  }
+  .active {
+    // overflow: visible;
+  }
+}
+</style>
